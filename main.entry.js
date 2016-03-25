f16fcc65a4a45a13d7a890f473992e9c779d07a4
unknown <raoenhui>
2016-03-25 18:03:38 +0800

    入门例子

---
+++ main.entry.js
@@ -0,0 +1,10 @@
+webpackJsonp([0,1],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	(function webpackMissingModule() { throw new Error("Cannot find module \"e:\\file\\webpack\\webpack\\entry.js\""); }());
+	(function webpackMissingModule() { throw new Error("Cannot find module \"bundle.js\""); }());
+
+
+/***/ }
+]);
